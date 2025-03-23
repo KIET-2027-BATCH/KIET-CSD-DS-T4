859aa0060fc169d6d8e876d78dccd99cf291d004
Satyanarayana2417 <168924054+Satyanarayana2417@users.noreply.github.com>
2025-03-23 19:31:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const API_CONFIG = {
+    BASE_URL: 'http://127.0.0.1:5000/api',
+    ENDPOINTS: {
+        PREDICT: '/predict',
+        CITIES: '/cities'
+    },
+    HEADERS: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+};
+
+export default API_CONFIG;
